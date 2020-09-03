4a56757012afe2a13ea658dd95b09367e68561bd
Arsal <arslan@akhtarsitsolutions.com>
2020-09-04 00:45:08 +0500

    title column

--- app/Http/Controllers/Admin/WebsiteController.php
+++ app/Http/Controllers/Admin/WebsiteController.php
@@ -92,7 +92,7 @@ class WebSiteController extends Controller
                     $uweb=new UserWebsite();
                     $uweb->website_id=$web->id;
                     $uweb->user_id=Auth::user()->id;
-                    $uweb->title='ABC';
+                    // $uweb->title='ABC';
                     $uweb->emails=$request->emails;
                     if(isset($request->ssl))
                     $uweb->ssl=1;
