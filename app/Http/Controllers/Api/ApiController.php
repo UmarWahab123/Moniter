bb90562d6f39b481ec73c63c8598f99b27f76f4c
Arsal <arslan@akhtarsitsolutions.com>
2020-12-28 12:20:43 +0500

    data array optimizaed

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -6,25 +6,85 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Monitor;
 use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Exceptions\TokenExpiredException;
+use Tymon\JWTAuth\Exceptions\TokenInvalidException;
+
 class ApiController extends Controller
 {
     protected $user;
  
     public function __construct()
     {
-        $this->user = JWTAuth::parseToken()->authenticate();
+        try 
+        {
+            if (! $this->user = JWTAuth::parseToken()->authenticate())
+            {
+                return response()->json([
+                    'error' => true,
+                    'code'  => 10,
+                    'data'  => [
+                        'message'   => 'User not found by given token'
+                    ]
+                ]);
+            }
+
+        } catch (TokenExpiredException $e) {
+            return response()->json([
+                'error' => true,
+                'code'  => 11,
+                'data'  => [
+                    'message'   => 'Token Expired'
+                ]
+            ]);
+
+        } catch (TokenInvalidException $e) {
+
+            return response()->json([
+                'error' => true,
+                'code'  => 12,
+                'data'  => [
+                    'message'   => 'Invalid Token'
+                ]
+            ]);
+
+        } catch (JWTException $e) {
+
+            return response()->json([
+                'error' => true,
+                'code'  => 13,
+                'data'  => [
+                    'message'   => 'Token absent'
+                ]
+            ]);
+
+        }
+
     }
     public function getMonitor(Request $request)
     {
+        $data=null;
         $website_ids=$this->user->userWebsites->pluck('website_id')->toArray();
-        $websites=Monitor::whereIn('id',$website_ids)->get()->toArray();
-        if (!$websites) {
+        $websites=Monitor::whereIn('id',$website_ids)->get();
+        foreach($websites as $website)
+        $data[]=array(
+            "title"=>($website->getSiteDetails!=null)?$website->getSiteDetails->title:'N/A',
+            "url"=>$website->url,
+            "status"=>$website->uptime_status,
+            "last_status_change"=>$website->uptime_status_last_change_date,
+            "ssl_check"=>($website->certificate_check_enabled==1)?'On':'Off',
+            "certificate_expiry_date"=>$website->certificate_expiration_date,
+            "certificate_issuer"=>$website->certificate_issuer,
+            "created_at"=>$website->created_at,
+            "updated_at"=>$website->updated_at,
+        );
+        if (!$data) {
             return response()->json([
-                'success' => false,
+                'success' => true,
                 'message' => 'No website against this user',
             ], 400);
         }
      
-        return $websites;
+        return $data;
     }
 }
