62c38c1eaf6996b0c2150d9ea77e26f53b199219
Akif Hussain <akif@knowpakistan.net>
2022-08-11 13:09:44 +0500

    curl check script

--- routes/web.php
+++ routes/web.php
@@ -201,3 +201,25 @@ Route::get('/set-admin-role-id-to-all-users', function()
     }
     return 'suucess';
 });
+Route::get('/curl_call_check', function()
+{
+    $url  = 'http://wstats.websiteuptimerobot.com/add_server_new';
+    $data['ip_address']    = '192.1923.23';
+    $data['user_id']       = 'usama';
+    $data['server_id']     = 'new';
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => $data,
+    ));
+    $response = curl_exec($curl);
+    curl_close($curl);
+    return json_decode($response);
+});
