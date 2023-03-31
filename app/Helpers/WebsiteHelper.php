419fbd1145250f11398234e2c5bfd13aa50415f5
Farooq khan <farooq@pkteam.com>
2023-03-31 11:14:42 +0500

    adding websites limit

--- app/Helpers/WebsiteHelper.php
+++ app/Helpers/WebsiteHelper.php
@@ -32,7 +32,7 @@ class WebsiteHelper
             return WebsiteHelper::WebsitesDatatable($query);
         }
         $no_of_websites_allowed = @auth()->user()->package->no_of_websites;
-        $user_website_added = UserWebsite::where('user_id', auth()->user()->id)->count();
+        $user_website_added = UserWebsite::where('user_id', auth()->user()->id)->orWhere('parent_id', auth()->user()->id)->count();
         $servers = Server::select('id', 'name')->where('user_id', Auth::user()->id)->get();
         $users = User::where('role_id', 2)->where('parent_id', Auth::user()->id)->select('id', 'name')->get();
         return view('admin.websites.index', compact('websites', 'servers', 'users','no_of_websites_allowed','user_website_added'));
